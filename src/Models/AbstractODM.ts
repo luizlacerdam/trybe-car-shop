dc4ac8aca7bea18568f3e068ae5641eb92162269
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-27 20:59:32 -0300

    :sparkles: [feat] CarODM

--- src/Models/AbstractODM.ts
+++ src/Models/AbstractODM.ts
@@ -5,7 +5,7 @@ export default abstract class AbstractODM<T> {
   protected _schema: Schema;
   protected _modelName: string;
     
-  constructor(schema: Schema, modelName: string) {
+  constructor(modelName: string, schema: Schema) {
     this._schema = schema;
     this._modelName = modelName;
     this.model = models[this._modelName] || model(this._modelName, this._schema);
