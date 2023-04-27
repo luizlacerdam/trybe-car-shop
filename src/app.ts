58594a741db5a09cc202da77b988e059da7d3f7a
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-27 18:21:07 -0300

    :recycle: [refa] refactor-app-server

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,5 @@
 import express from 'express';
 
-class App {
-  public app: express.Express;
-  constructor() {
-    this.app = express();
-    // this.config();
+const app = express();
 
-    this.app.get('/', (req, res) => res.json({ ok: true }));
-  }
-  public start(PORT: string | number):void {
-    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
-  }
-}
-
-export { App };
-
-export const { app } = new App();
+export default app;
