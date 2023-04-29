f19ee99d8e10991df8d11728a549de4d109b9793
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-04-29 15:12:22 -0300

    :sparkles: [feat] IVehicle-Domain

--- src/Domains/Motorcycle.ts
+++ src/Domains/Motorcycle.ts
@@ -1,22 +1,10 @@
 import IMotorcycle from '../Interfaces/IMotorcycle';
 
 export default class Motorcycle {
-  protected id: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status: boolean | undefined;
-  protected buyValue: number;
   private category: 'Street' | 'Custom' | 'Trail';
   private engineCapacity: number;
 
   constructor(car: IMotorcycle) {
-    this.id = car.id;
-    this.model = car.model;
-    this.year = car.year;
-    this.color = car.color;
-    this.status = car.status || false;
-    this.buyValue = car.buyValue;
     this.category = car.category;
     this.engineCapacity = car.engineCapacity;
   }
---
+++ src/Domains/Vehicle.ts
@@ -0,0 +1,20 @@
+import ICar from '../Interfaces/ICar';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+
+export default class Vehicle {
+  protected id: string | undefined;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status: boolean | undefined;
+  protected buyValue: number;
+
+  constructor(vehicle: ICar | IMotorcycle) {
+    this.id = vehicle.id;
+    this.model = vehicle.model;
+    this.year = vehicle.year;
+    this.color = vehicle.color;
+    this.status = vehicle.status || false;
+    this.buyValue = vehicle.buyValue;
+  }
+}
