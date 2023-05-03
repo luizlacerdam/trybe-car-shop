12cfed2afe7c9d547ae86a25b7c76a6ef7fe2297
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-03 20:57:35 -0300

    :recycle: [refactor] VehicleFactory

---
+++ src/Domains/VehicleFactory.ts
@@ -0,0 +1,17 @@
+import ICar from '../Interfaces/ICar';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import VehicleTypes from '../utils/VehicleTypes';
+import Car from './Car';
+import Motorcycle from './Motorcycle';
+
+export default class VehicleFactory {
+  public static createDomain<T>(type: string, vehicle: T) {
+    if (type === VehicleTypes.CAR) {
+      return new Car(vehicle as unknown as ICar);
+    }
+    if (type === VehicleTypes.MOTORCYCLE) {
+      return new Motorcycle(vehicle as unknown as IMotorcycle);
+    }
+    return new Error('Invalid vehicle type!');
+  }
+}
