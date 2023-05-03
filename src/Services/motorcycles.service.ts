7f95af4cb6f24197cfc06498568377a8da843dc1
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-03 20:30:39 -0300

    :recycle: [refactor] refatoracao-service-done

--- src/Services/motorcycles.service.ts
+++ src/Services/motorcycles.service.ts
@@ -15,27 +15,27 @@ export default class MotocyclesService implements IService<IMotorcycle, Motorcyc
     return null;
   }
   public async create(vehicle: IMotorcycle):Promise<Motorcycle | null> {
-    const motorcyclesODM = new MotorcyclesODM();
+    const motorcyclesODM = this._domain;
     const newMoto = await motorcyclesODM.create(vehicle);
     return this.newDomain(newMoto);
   }
 
   public async getAll():Promise<(Motorcycle | null)[]> {
-    const motorcyclesODM = new MotorcyclesODM();
+    const motorcyclesODM = this._domain;
     const motorcycles = await motorcyclesODM.find();    
     const motorcyclesArray = motorcycles.map((moto) => this.newDomain(moto));
     return motorcyclesArray;
   }
 
   public async getById(id: string):Promise<Motorcycle | null> {
-    const motorcyclesODM = new MotorcyclesODM();
+    const motorcyclesODM = this._domain;
     const data = await motorcyclesODM.findById(id);
     if (!data) return null;
     return this.newDomain(data);
   }
 
   public async update(id: string, body: IMotorcycle): Promise<Motorcycle | null> {
-    const motorcyclesODM = new MotorcyclesODM();
+    const motorcyclesODM = this._domain;
     const data = await motorcyclesODM.update(id, body);
     if (!data) return null;
     return this.newDomain(data);
