d7a3ff1a0fb6305401d76723eda0bd304edcedbb
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-04 16:13:13 -0300

    :recycle: [refactor] app-and-server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,30 @@
+// import express from 'express';
+// import carsRoutes from './Routes/cars.routes';
+// import motorcycleRouter from './Routes/motorcycles.route';
+
+// const app = express();
+// app.use(express.json());
+// app.use('/motorcycles', motorcycleRouter);
+// app.use('/cars', carsRoutes);
+
+// export default app;
 import express from 'express';
-import carsRoutes from './Routes/cars.routes';
 import motorcycleRouter from './Routes/motorcycles.route';
+import carsRoutes from './Routes/cars.routes';
 
-const app = express();
-app.use(express.json());
-app.use('/motorcycles', motorcycleRouter);
-app.use('/cars', carsRoutes);
+export default class App {
+  public app: express.Express;
 
-export default app;
+  constructor() {
+    this.app = express();
+    
+    this.app.get('/', (req, res) => res.json({ ok: true }));
+    this.app.use(express.json());
+    this.app.use('/motorcycles', motorcycleRouter);
+    this.app.use('/cars', carsRoutes);
+    // this.app.use();
+  }
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+  }
+}
