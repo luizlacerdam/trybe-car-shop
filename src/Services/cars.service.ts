e2bcef7f7cb9baceb540f8131a383e076d8284ed
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-05-04 10:03:12 -0300

    :recycle: [refactor] Car-and-Motorcycle-services-refa

--- src/Services/cars.service.ts
+++ src/Services/cars.service.ts
@@ -1,43 +1,10 @@
-import Car from '../Domains/Car';
 import ICar from '../Interfaces/ICar';
-import IService from '../Interfaces/IService';
 import CarsODM from '../Models/CarsODM';
+import VehicleTypes from '../utils/VehicleTypes';
+import AbstractService from './AbstractService';
 
-export default class CarsService implements IService<ICar, Car> {
-  private _domain: CarsODM;
+export default class CarsService extends AbstractService<ICar> {
   constructor(domain: CarsODM) {
-    this._domain = domain;
-  }
-  private newDomain(vehicle: ICar | null): Car | null {
-    if (vehicle) {
-      return new Car(vehicle);
-    }
-    return null;
-  }
-  public async create(vehicle: ICar):Promise<Car | null> {
-    const carsODM = this._domain;
-    const newCar = await carsODM.create(vehicle);
-    return this.newDomain(newCar);
-  }
-
-  public async getAll():Promise<(Car | null)[]> {
-    const carsODM = this._domain;
-    const cars = await carsODM.find();    
-    const carsArray = cars.map((car) => this.newDomain(car));
-    return carsArray;
-  }
-
-  public async getById(id: string):Promise<Car | null> {
-    const carsODM = this._domain;
-    const data = await carsODM.findById(id);
-    if (!data) return null;
-    return this.newDomain(data);
-  }
-
-  public async update(id: string, body: ICar): Promise<Car | null> {
-    const carsODM = this._domain;
-    const data = await carsODM.update(id, body);
-    if (!data) return null;
-    return this.newDomain(data);
+    super(domain, VehicleTypes.CAR);
   }
 }
--- src/Services/motorcycles.service.ts
+++ src/Services/motorcycles.service.ts
@@ -1,43 +1,10 @@
-import Motorcycle from '../Domains/Motorcycle';
 import IMotorcycle from '../Interfaces/IMotorcycle';
-import IService from '../Interfaces/IService';
 import MotorcyclesODM from '../Models/MotorcyclesODM';
+import VehicleTypes from '../utils/VehicleTypes';
+import AbstractService from './AbstractService';
 
-export default class MotocyclesService implements IService<IMotorcycle, Motorcycle> {
-  private _domain: MotorcyclesODM;
+export default class MotorcycleService extends AbstractService<IMotorcycle> {
   constructor(domain: MotorcyclesODM) {
-    this._domain = domain;
-  }
-  private newDomain(vehicle: IMotorcycle | null): Motorcycle | null {
-    if (vehicle) {
-      return new Motorcycle(vehicle);
-    }
-    return null;
-  }
-  public async create(vehicle: IMotorcycle):Promise<Motorcycle | null> {
-    const motorcyclesODM = this._domain;
-    const newMoto = await motorcyclesODM.create(vehicle);
-    return this.newDomain(newMoto);
-  }
-
-  public async getAll():Promise<(Motorcycle | null)[]> {
-    const motorcyclesODM = this._domain;
-    const motorcycles = await motorcyclesODM.find();    
-    const motorcyclesArray = motorcycles.map((moto) => this.newDomain(moto));
-    return motorcyclesArray;
-  }
-
-  public async getById(id: string):Promise<Motorcycle | null> {
-    const motorcyclesODM = this._domain;
-    const data = await motorcyclesODM.findById(id);
-    if (!data) return null;
-    return this.newDomain(data);
-  }
-
-  public async update(id: string, body: IMotorcycle): Promise<Motorcycle | null> {
-    const motorcyclesODM = this._domain;
-    const data = await motorcyclesODM.update(id, body);
-    if (!data) return null;
-    return this.newDomain(data);
+    super(domain, VehicleTypes.MOTORCYCLE);
   }
 }
